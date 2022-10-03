81e5adf74181c642de27f3cd90c75456b5be2ea7
John Kosmicke <kozjob@yahoo.com>
2022-10-03 16:07:33 -0700

    set 2 encoding/decoding

--- 002/code.py
+++ 002/code.py
@@ -1,6 +1,26 @@
 
+import os, sys
+ROOT_DIR = os.path.abspath(os.curdir)
+UTIL_PATH = os.path.join( ROOT_DIR, 'utils' )
+sys.path.append(UTIL_PATH)
+
+import code
+from const import letter_dict, number_dict
+
+
 def encode_string(s, a, b):
     ret_arr = []
     for c in s:
-        pass
+        ret_arr.append( letter_dict[c] * a + b )
+    return ret_arr
 
+
+def decode_numbers(arr, a, b):
+    ret_s =  ''
+    for elt in arr:
+        d = (elt - b ) // a
+        if (elt - b) % a !=0 or d not in number_dict:
+            return False
+        else:
+            ret_s += number_dict[d]
+    return ret_s
--- 002/tests.py
+++ 002/tests.py
@@ -6,10 +6,25 @@ sys.path.append(UTIL_PATH)
 import code
 from unit_test import unit_test
 
+
 test_values = [
+    (code.encode_string,  [1, 0, 3, 1, 20], ('a cat', 1, 0 ) ), 
+    (code.encode_string,  [5, 3, 9, 5, 43], ('a cat', 2, 3 ) ), 
+    (code.decode_numbers,  'a cat', ([5, 3, 9, 5, 43], 2, 3) )
 ]
 
 print()
 for t in test_values:
     f, value, arg_tuple = t
     unit_test(f, arg_tuple, value)
+
+
+
+message = [377, 717, 71, 513, 105, 921, 581, 547, 547, 105, 377, 717, 241, 71, 105, 547, 71,
+377, 547, 717, 751, 683, 785, 513, 241, 547, 751]
+
+for a in range(1, 100):
+    for b in range(100):
+        decoded_message =code. decode_numbers( message, a, b )
+        if decoded_message:
+            print(decoded_message)
