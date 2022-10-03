0f21500d1074a2402f3199c51567f42c04f703b7
John Kosmicke <kozjob@yahoo.com>
2022-10-03 10:10:44 -0700

    cleanup import stuff

--- 001/tests.py
+++ 001/tests.py
@@ -1,25 +1,20 @@
 
+import os, sys
+ROOT_DIR = os.path.abspath(os.curdir)
+UTIL_PATH = os.path.join( ROOT_DIR, 'utils' )
+sys.path.append(UTIL_PATH)
 
-from code import *
-# from unit_test import unit_test
-
-def unit_test(function_name, arg_tuple, value):
-    print('testing ', function_name.__name__, ' on input ', arg_tuple, ' to be ', value, sep='')
-    if function_name(*arg_tuple) == value:
-        print('PASSED')
-    else:
-        print('FAILED')
-    print()
+import code
+from unit_test import unit_test
 
 test_values = [
-    (check_for_symmetry, True, ('racecar', ) ),
-    (convert_to_letters,  'a cat', ([1, 0, 3, 1, 20], ) ), 
-    (convert_to_numbers, [ 2, 1, 20, 13, 1, 14 ], ('batman', ) ),
-    (is_prime, True,  (59, ) ),
-    (is_prime, False, (51, ) )
+    (code.check_for_symmetry, True, ('racecar', ) ),
+    (code.convert_to_letters,  'a cat', ([1, 0, 3, 1, 20], ) ), 
+    (code.convert_to_numbers, [ 2, 1, 20, 13, 1, 14 ], ('batman', ) ),
+    (code.is_prime, True,  (59, ) ),
+    (code.is_prime, False, (51, ) )
 ]
 
 for t in test_values:
     f, value, arg_tuple = t
     unit_test(f, arg_tuple, value)
-
