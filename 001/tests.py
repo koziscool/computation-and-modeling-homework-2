a095cb7c8a1df847a21e1440917c34dc4abc054e
John Kosmicke <kozjob@yahoo.com>
2022-10-03 08:53:41 -0700

    remaining exercises and tests

--- 001/tests.py
+++ 001/tests.py
@@ -1,6 +1,6 @@
 
 
-from code import check_for_symmetry
+from code import *
 # from unit_test import unit_test
 
 def unit_test(function_name, arg_tuple, value):
@@ -11,12 +11,15 @@ def unit_test(function_name, arg_tuple, value):
         print('FAILED')
     print()
 
-symmetric_test_values = [
-    (check_for_symmetry, True, ('racecar', ) )
+test_values = [
+    (check_for_symmetry, True, ('racecar', ) ),
+    (convert_to_letters,  'a cat', ([1, 0, 3, 1, 20], ) ), 
+    (convert_to_numbers, [ 2, 1, 20, 13, 1, 14 ], ('batman', ) ),
+    (is_prime, True,  (59, ) ),
+    (is_prime, False, (51, ) )
 ]
 
-for t in symmetric_test_values:
+for t in test_values:
     f, value, arg_tuple = t
     unit_test(f, arg_tuple, value)
-
 
